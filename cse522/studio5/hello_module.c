08405cf44a27adeb06eb57c2216cf33ec0677fde
Robert Miller <millerrt3@gmail.com>
2016-02-04 15:48:34 -0600

    Repo reorganized

---
+++ cse522/studio5/hello_module.c
@@ -0,0 +1,24 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+
+static int hello_init(void)
+{
+  printk(KERN_ALERT "I bear a charmed life.\n");
+}
+
+
+static void hello_exit(void)
+{
+  printk(KERN_ALERT "Out, out brief candle!\n");
+}
+
+
+module_init(hello_init);
+module_exit(hello_exit);
+
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Shakespeare");
+MODULE_DESCRIPTION("A Hello, World Module");
