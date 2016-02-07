cc8890d9e7069d15d057f830804a24b927b14450
Cameron Whipple <whipple.cameron89@gmail.com>
2016-02-07 14:57:10 -0600

    Added script for setting up environment for cross compilation on ubuntu system. Cleaned up the cross_compile.sh file.

--- cse522/cross_compile.sh
+++ cse522/cross_compile.sh
@@ -1,37 +1,58 @@
 #!/bin/bash
 
+CSE522_DIR=`pwd`
 CSV_NAME="cse522_build_times.csv"
+LINUX_ROOT="$CSE522_DIR/../linux"
+LOGS_DIR="$CSE522_DIR/logs"
+BUILD_LOG_TEMPLATE="cross_compile"
 
-DATE_STRING=`date +"%m-%d-%y-%H-%M"`
+DATE_STRING=`date +"%m-%d-%Y-%H-%M"`
 KERNEL=kernel7
 
-mkdir -p logs
+# build the log directory
+mkdir $LOGS_DIR >> /dev/null 2>&1
+
+# create the file for logs
+BUILD_LOG=$LOGS_DIR"/"$DATE_STRING"_"$BUILD_LOG_TEMPLATE".log"
+touch $BUILD_LOG
+
+# Print banner
+clear
+echo "---------------------------------------------------------" | tee -a $BUILD_LOG 2>&1
+echo "            Cross Compile - $DATE_STRING                 " | tee -a $BUILD_LOG 2>&1
+echo "---------------------------------------------------------" | tee -a $BUILD_LOG 2>&1
+echo "Linux Root: "$LINUX_ROOT                                   | tee -a $BUILD_LOG 2>&1
+echo "Logs Root: "$LOGS_DIR                                      | tee -a $BUILD_LOG 2>&1
+echo "CSV Location: "$LOGS_DIR"/"$CSV_NAME                       | tee -a $BUILD_LOG 2>&1
+echo ""                                                          | tee -a $BUILD_LOG 2>&1
 
 # Generate CSV to keep track of build times
-if [ ! -f logs/$CSV_NAME ]; then
-    touch logs/$CSV_NAME
-    echo "\"Start Time\",\"End Time\",\"Elapsed Seconds\"" >> "logs/"$CSV_NAME
+cd $CSE522_DIR
+if [ ! -f $LOGS_DIR/$CSV_NAME ]; then
+    touch $LOGS_DIR/$CSV_NAME
+    echo "\"Start Time\",\"End Time\",\"Elapsed Seconds\",\"Build Log\"" >> $LOGS_DIR/$CSV_NAME
 fi
 
-cd ..
-
-echo "Cleaning"
+echo "Cleaning Build Tree"
+cd $LINUX_ROOT
 make clean
 
 echo "Performing Cross Compile"
 START_SECONDS=`date +"%s"`
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- -j4 zImage modules dtbs | tee "cse522/logs/"$DATE_STRING"_cross_compile.log" 2>&1
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- -j8 zImage modules dtbs | tee -a $BUILD_LOG 2>&1
 END_SECONDS=`date +"%s"`
 
 # Calculate Elapsed Seconds
 ELAPSED_SECONDS=`expr $END_SECONDS - $START_SECONDS`
-
 START_DATE=`date --date="@$START_SECONDS"`
 END_DATE=`date --date="@$START_SECONDS"`
 
 # Export time to the csv
-echo "\"$START_DATE\",\"$END_DATE\",\"$ELAPSED_SECONDS\"" >> "cse522/logs/"$CSV_NAME
+
+echo "Exporting Records - See "$LOGS_DIR"/"$CSV_NAME
+echo "\"$START_DATE\",\"$END_DATE\",\"$ELAPSED_SECONDS\",\"$BUILD_LOG\"" >> $LOGS_DIR"/"$CSV_NAME
 
 echo "Start: "$START_SECONDS", End: "$END_SECONDS", Elapsed: "$ELAPSED_SECONDS
 
-cd cse522
+# return to origin directory
+cd $CSE522_DIR
