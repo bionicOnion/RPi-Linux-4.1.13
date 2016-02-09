64a69d27eb33e22bf23d2d529b094ca503d753a5
Cameron Whipple <whipple.cameron89@gmail.com>
2016-02-09 15:56:54 -0600

    Completed Lab 1, Step 2

--- cse522/lab1/spawn.c
+++ cse522/lab1/spawn.c
@@ -1,24 +1,51 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/kthread.h>
+
+/* -- holds reference to kernel thread that was created -- */
+struct task_struct* pThread = 0;
+
+/* -- entry point for the spawned thread -- */
+static int threadEntryPoint( void *data )
+{
+
+	printk( KERN_DEBUG "Spawned thread started\n" );
+
+//	while(1)
+//	{
+		printk( KERN_DEBUG "threadEntryPoint\n" );
+		schedule();
+//	}
+
+	return 0;
+}
 
 
 static int spawn_init(void)
 {
-   printk(KERN_DEBUG "spawn_init\n" );
+	printk(KERN_DEBUG "spawn_init\n" );
+
+	/* -- creates and runs the thread -- */
+	pThread = kthread_run(&threadEntryPoint, NULL, "Framework Monitor");
+	if( pThread == NULL )
+	{
+		printk( KERN_ALERT "KThread_Create Failed\n" );
+		return -1;
+	}
+
 	return 0;
 }
 
 
 static void spawn_exit(void)
 {
-   printk(KERN_DEBUG "spawn_exit\n" );
+	printk(KERN_DEBUG "spawn_exit\n" );
 }
 
 
 module_init(spawn_init);
 module_exit(spawn_exit);
-
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Cameron Whipple and Robert Miller");
