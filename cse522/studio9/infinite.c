04541839662247b542fded5287ce30989a3e934a
Robert Miller <millerrt3@gmail.com>
2016-02-29 22:49:29 -0600

    Studio 9 complete

---
+++ cse522/studio9/infinite.c
@@ -0,0 +1,49 @@
+#define _GNU_SOURCE
+
+
+#include <math.h>
+#include <sched.h>
+#include <stdio.h>
+
+
+int main(int argc, char** argv)
+{
+	if (argc < 4)
+	{
+		printf("Usage: %s [core number] [priority] [process count]\n", argv[0]);
+		return 1;
+	}
+
+	// Pin to the specified CPU
+	int requestedCPU = atoi(argv[1]);
+	printf("%d\n", requestedCPU);
+	cpu_set_t cpuSet;
+	CPU_ZERO(&cpuSet);
+	CPU_SET(requestedCPU, &cpuSet);
+	sched_setaffinity(getpid(), sizeof(cpuSet), &cpuSet);
+
+	// Use the specified scheduling model
+	int requestedPriority = atoi(argv[2]);
+	struct sched_param schedParam;
+	schedParam.sched_priority = requestedPriority;
+	if (sched_setscheduler(0, SCHED_RR, &schedParam) != 0)
+	{
+		printf("Failed to set the requested scheduler priority.\n");
+		return 2;
+	}
+
+	// Fork off the specified number of real-time processes
+	int requestedProcessCount = atoi(argv[3]);
+	int i;
+	for (i = 1; i < requestedProcessCount; ++i)
+	{
+		if (fork() == 0)
+			break;
+		else printf("CHILD\n");
+	}
+	
+	// Perform 'work'
+	for(i = 0; i < 500000000; ++i);
+
+	return 0;
+}
